14ea83d527ce6e309e5a7842e67b4c9dc631e2bc
Christopher Kebschull <christopher@okapiorbits.com>
2021-09-23 15:42:26 +0200

    Reactivate JB2008 indices file download

--- work/start_neptune.sh
+++ work/start_neptune.sh
@@ -3,24 +3,24 @@
 ln -sf ../bin/neptune-sa
 #
 # Download solar and geomagnetic activity data, when the files are older than 1 da#
-if [[ `find "data/fap_day.dat" -mtime +24` ]]; then
+if [[ `find "data/fap_day.dat" -mtime +0` ]]; then
   wget -N https://static.sdo.esoc.esa.int/SOLMAG/fap_day.dat --directory-prefix=./data
   wget -N https://static.sdo.esoc.esa.int/SOLMAG/fap_mon.dat --directory-prefix=./data
 fi
-if [[ `find "data/sw19571001.txt" -mtime +24` ]]; then
+if [[ `find "data/sw19571001.txt" -mtime +0` ]]; then
   wget -N https://celestrak.com/SpaceData/SW-All.txt --directory-prefix=./data
   mv ./data/SW-All.txt ./data/sw19571001.txt
 fi
-if [[ `find "data/eop19620101.txt" -mtime +24` ]]; then
+if [[ `find "data/eop19620101.txt" -mtime +0` ]]; then
   wget -N https://celestrak.com/SpaceData/EOP-All.txt --directory-prefix=./data
   mv ./data/EOP-All.txt ./data/eop19620101.txt
 fi
-#if [[ `find "data/SOLFSMY.TXT" -mtime +24` ]]; then
-#  wget -N http://sol.spacenvironment.net/~JB2008/indices/SOLFSMY.TXT --directory-prefix=./data
-#fi
-#if [[ `find "data/DTCFILE.TXT" -mtime +24` ]]; then
-#  wget -N http://sol.spacenvironment.net/~JB2008/indices/DTCFILE.TXT --directory-prefix=./data
-#fi
+if [[ `find "data/SOLFSMY.TXT" -mtime +0` ]]; then
+  wget -N http://sol.spacenvironment.net/~JB2008/indices/SOLFSMY.TXT --directory-prefix=./data
+fi
+if [[ `find "data/DTCFILE.TXT" -mtime +0` ]]; then
+  wget -N http://sol.spacenvironment.net/~JB2008/indices/DTCFILE.TXT --directory-prefix=./data
+fi
 #
 # Create the output directory if it does not exist
 #
