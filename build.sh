3cff9a1ec8f8c725ff57d4f69954e10783cdae38
Christopher Kebschull <christopher@okapiorbits.com>
2023-01-12 09:19:25 +0100

    Optimize build.sh

--- build.sh
+++ build.sh
@@ -20,14 +20,14 @@ git submodule update --init --recursive
 #                                                                              #
 ################################################################################
 # Build OPI
-cd OPI
+cd OPI || exit
 # Create the build directory if it does not exist
 if [[ ! -d "build" ]]; then
   mkdir build
 else
   rm -rf build/*
 fi
-cd build
+cd build || exit
 echo "Updating cmake"
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=../ -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_CXX11=ON -DENABLE_CL_SUPPORT=OFF -DENABLE_CUDA_SUPPORT=OFF -DENABLE_PYTHON=OFF ../
 echo "Building OPI"
@@ -44,14 +44,14 @@ cd ../../
 #                                                                              #
 ################################################################################
 # Build libslam
-cd libslam
+cd libslam || exit
 # Create the build directory if it does not exist
 if [[ ! -d "build" ]]; then
   mkdir build
 else
   rm -rf build/*
 fi
-cd build
+cd build || exit
 echo "Updating cmake"
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_OpenMP_SUPPORT=OFF -DENABLE_POSTGRESQL_SUPPORT=OFF ../
 echo "Building libslam"
@@ -71,17 +71,17 @@ cd ../
 #                                Build pFUnit                                  #
 #                                                                              #
 ################################################################################
-cd pFUnit
+cd pFUnit || exit
 # Create the build directory if it does not exist
 if [[ ! -d "build" ]]; then
   mkdir build
 else
   rm -rf build/*
 fi
-cd build
+cd build || exit
 echo "Updating cmake"
-cmake  -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER ../
-cmake  -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER ../
+export FC=$Fortran_COMPILER
+cmake -DSKIP_MPI=yes ../
 echo "Building pFUnit"
 make
 make install
@@ -98,7 +98,7 @@ if [[ ! -d "lib" ]]; then
   mkdir lib
 fi
 # Create the links to libraries needed by CAMP
-cd lib
+cd lib || exit
 ln -sf ../libslam/lib/libslam-Fortran.$LIBSUFFIX .
 ln -sf ../OPI/lib/libOPI-Fortran.$LIBSUFFIX .
 ln -sf ../OPI/lib/libOPI.$LIBSUFFIX .
@@ -108,7 +108,7 @@ if [[ ! -d "include" ]]; then
   mkdir include
 fi
 # Create the links to includes needed by NEPTUNE
-cd include
+cd include || exit
 ln -sf ../libslam/include/SLAM .
 ln -sf ../OPI/include/OPI .
 cd ..
@@ -118,7 +118,7 @@ if [[ ! -d "build" ]]; then
 else
   rm -rf build/*
 fi
-cd build
+cd build || exit
 echo "Updating cmake"
 export PFUNIT_DIR=..//pFUnit/build/installed
 cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_OPI_SUPPORT=ON ../
@@ -129,7 +129,7 @@ if [[ $? -ne 0 ]]; then
     exit $?
 fi
 echo "Leaving NEPTUNE"
-cd ../work
-ln -sf ../bin/neptune-sa
+cd ../work || exit
+ln -sf ../bin/neptune-sa .
 cd ..
 echo "Done"
