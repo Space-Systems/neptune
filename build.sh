e8accd073c0a0e1fdb08bd0fb7594f5bb955d561
Christopher Kebschull <christopher@okapiorbits.com>
2023-06-02 16:58:18 +0900

    Fix typo in build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # Define how to build the libraries and executables:
 BUILD_TYPE=Debug
 Fortran_COMPILER=gfortran
-GENERATOR="GNU Makefiles"
+GENERATOR="Unix Makefiles"
 ENABLE_OPI_SUPPORT=OFF
 LIBSUFFIX="so"
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -35,7 +35,7 @@ if [[ "$ENABLE_OPI_SUPPORT" == "ON" ]]; then
   fi
   cd build || exit
   echo "Updating cmake"
-  cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=../ -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_CXX11=ON -DENABLE_CL_SUPPORT=OFF -DENABLE_CUDA_SUPPORT=OFF -DENABLE_PYTHON=OFF -G $GENERATOR ../
+  cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_INSTALL_PREFIX=../ -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_CXX11=ON -DENABLE_CL_SUPPORT=OFF -DENABLE_CUDA_SUPPORT=OFF -DENABLE_PYTHON=OFF -G "$GENERATOR" ../
   echo "Building OPI"
   cmake --build .
   cmake --install .
@@ -61,7 +61,7 @@ else
 fi
 cd build || exit
 echo "Updating cmake"
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_OpenMP_SUPPORT=OFF -DENABLE_POSTGRESQL_SUPPORT=OFF -G $GENERATOR ../
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_OpenMP_SUPPORT=OFF -DENABLE_POSTGRESQL_SUPPORT=OFF -G "$GENERATOR" ../
 echo "Building libslam"
 cmake --build .
 cmake --install .
@@ -80,23 +80,23 @@ cd ../
 #                                Build pFUnit                                  #
 #                                                                              #
 ################################################################################
-# cd pFUnit || exit
-# # Create the build directory if it does not exist
-# if [[ ! -d "build" ]]; then
-#   mkdir build
-# else
-#   rm -rf build/*
-# fi
-# cd build || exit
-# echo "Updating cmake"
-# export FC=$Fortran_COMPILER
-# cmake -DSKIP_MPI=yes -G $GENERATOR ../
-# echo "Building pFUnit"
-# cmake --build .
-# cmake --install .
-# cd ../
-# echo "Leaving pFUnit"
-# cd ../
+cd pFUnit || exit
+# Create the build directory if it does not exist
+if [[ ! -d "build" ]]; then
+  mkdir build
+else
+  rm -rf build/*
+fi
+cd build || exit
+echo "Updating cmake"
+export FC=$Fortran_COMPILER
+cmake -DSKIP_MPI=yes -G "$GENERATOR" ../
+echo "Building pFUnit"
+cmake --build .
+cmake --install .
+cd ../
+echo "Leaving pFUnit"
+cd ../
 ################################################################################
 #                                                                              #
 #                                 Build NEPTUNE                                #
@@ -134,7 +134,7 @@ fi
 cd build || exit
 echo "Updating cmake"
 export PFUNIT_DIR=..//pFUnit/build/installed
-cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_OPI_SUPPORT=$ENABLE_OPI_SUPPORT -DSKIP_MSIS_2=ON -G $GENERATOR ../
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DCMAKE_Fortran_COMPILER=$Fortran_COMPILER -DENABLE_OPI_SUPPORT=$ENABLE_OPI_SUPPORT -DSKIP_MSIS_2=ON -G "$GENERATOR" ../
 echo "Building NEPTUNE"
 cmake --build .
 cmake --install .
